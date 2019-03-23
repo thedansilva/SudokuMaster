0db15c12dd09864eb732762d6718f2d5f2719879
thedansilva <43654534+thedansilva@users.noreply.github.com>
2019-03-23 13:14:16 -0400

    closes #4

--- giveUp.js
+++ giveUp.js
@@ -6,8 +6,8 @@ function giveUp() {
     document.getElementById("submitButton").disabled = true;
     var keyAlert = "";
     var y, x, j = 0;
-    for(x = 0; x < 8; x++) {
-    for(y = 0; y < 8; y++) {
+    for(x = 0; x < 9; x++) {
+    for(y = 0; y < 9; y++) {
       keyAlert += key[j] + " ";
       j++;
     }
