6037464bfb350d6a431f0804c96cb5c8885235ad
thedansilva <43654534+thedansilva@users.noreply.github.com>
2019-03-22 22:14:31 -0400

    added leaderboards and addscore.js
    addscore.js might be deprecated by submit.php, will re-evaluate later
    still need lboard2.txt up to and including lboard10.txt, will be uploaded by another contributor as per request

--- addScore.js
+++ addScore.js
@@ -1,31 +1,18 @@
 function addScore() {
   //  var inputs = document.getElementById("form").getElementsByTagName("input");
-    var username = document.getElementById('username').value;
-    if(username.length < 3) {
-      alert("Please type a FULL inital (3 characters).");
-      return false;
-    }
-    var tablegrab = document.getElementsByTagName('span');
-    var userscore = document.getElementById('userscore').value;
+    var inputs = document.querySelectorAll('input[type="number"]');
+    var answer = document.getElementById('answer').innerHTML;
     var level = document.getElementById('level').innerHTML;
-    var scores = new Array(9);// there are NINE other names on the board
-    var names = new Array(9); // excluding the user which we will add later
-    var x = 0;
-    for(x = 0; x < 9; x++) {
-      names[x] = tablegrab[x * 2];
-      scores[x] = tablegrab[(x * 2) + 1];
+    var key = answer.split(" "); // arrayify answer by delimiting spaces
+    document.getElementById("submitButton").disabled = true;
+    var keyAlert = "";
+    var y, x, j = 0;
+    for(x = 0; x < 8; x++) {
+    for(y = 0; y < 8; y++) {
+      keyAlert += key[j] + " ";
+      j++;
     }
-    x = 0;
-    var output = "";
-    var position = 0;
-    var scoreposted = false;
-    for(x = 0; x < 9; x++) {
-      if (userscore > parseInt(scores[x].innerHTML) && !scoreposted) {
-        output += username + " " + userscore + " " + "\n";
-        scoreposted = true;
-      } else {
-        output += names[position].innerHTML + " " + scores[position].innerHTML + " " + "\n";
-        position++;
-      }
-    }
+    keyAlert += "\n";
+  }
+  alert(keyAlert);
 }
